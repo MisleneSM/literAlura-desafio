9660f2fa2549dc8ac8d417c50dddaa399279b96c
Mislene Moura <mislene.moura2000@gmail.com>
2024-05-16 13:00:35 -0300

    Migrando arquivo spring com Github

---
+++ src/main/java/br/com/alura/literalura/LiteraluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.literalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+}
