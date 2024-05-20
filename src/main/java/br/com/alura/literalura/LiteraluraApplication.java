bc2c2ef4aabf8a95607b5a64d21eb75fb891c152
Mislene Moura <mislene.moura2000@gmail.com>
2024-05-20 16:08:00 -0300

    Feito chamada API e obtendo dados,. Iniciado a conversão de json para classe porém no momento sem sucesso de retorno bem sucedido.

--- src/main/java/br/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/br/com/alura/literalura/LiteraluraApplication.java
@@ -1,10 +1,27 @@
 package br.com.alura.literalura;
 
+import br.com.alura.literalura.model.DadosLivros;
+import br.com.alura.literalura.service.ConsumoApi;
+import br.com.alura.literalura.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("http://gutendex.com/books/?search=frankenstein+or+the+modern+prometheus");
+		System.out.println("Resultado" + json);
+
+//		//verificarApi.buscarLivrosApi();
+//		System.out.println(verificarApi.buscarLivrosApi());
+
+		ConverteDados conversor = new ConverteDados();
+		DadosLivros dados = conversor.obterDados(json, DadosLivros.class);
+		System.out.println(dados);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
---
+++ src/main/java/br/com/alura/literalura/principal/Principal.java
@@ -0,0 +1,11 @@
+//package br.com.alura.literalura.principal;
+//
+//import java.util.Scanner;
+//
+//public class Principal {
+//    private final String ENDERECO = "";
+//    Scanner scanner = new Scanner(System.in);
+//    public void exibeMenu(){
+//        System.out.println("");
+//    }
+//}
